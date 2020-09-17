22a6b675498b464b017b2895a07ef63753f483c7
Thomas Munzer <thomas.munzer@gmail.com>
2020-09-17 19:35:54 +0200

    update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mistsystems",
-    version="0.2.8",
+    version="0.2.9",
     author="Thomas Munzer",
     author_email="tmunzer@juniper.net",
     description="Python Library to use Juniper-Mist APIs",
